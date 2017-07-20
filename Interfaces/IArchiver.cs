3535aec25d2056a3fc6937aa07c848e8a2de5021
Siketyan <siketyan@sikeserver.com>
2017-07-20 17:56:19 +0900

    Add a archiver interface

---
+++ Interfaces/IArchiver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Defroster.Interfaces
+{
+    public interface IArchiver
+    {
+        string Name { get; }
+        string[] Extensions { get; }
+
+        event EventHandler<int> ProgressChanged;
+
+        void Defrost(string path);
+    }
+}
