0b6b4f146de8db81fb2653b10349b375a18950c5
Siketyan <siketyan@sikeserver.com>
2017-07-20 16:31:43 +0900

    Initial solution

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Defroster
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Windows/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Defroster
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
